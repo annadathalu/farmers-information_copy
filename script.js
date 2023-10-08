e6d01b733cad0d3421b9d14f6a407a602cc3e928
annadathalu <147304039+annadathalu@users.noreply.github.com>
2023-10-08 17:59:33 +0530

    Update script.js to telugu file

--- script.js
+++ script.js
@@ -5,24 +5,62 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Function to fetch data based on selected language
     function fetchData(selectedLanguage) {
-        let csvFileName = "";
+        let fileName = "";
+        let columnNames = {};
 
         if (selectedLanguage === "eng") {
-            csvFileName = "Data_v2_mapped.csv";
+            fileName = "Data_v2_mapped.csv";
+            // Define column names for English language
+            columnNames = {
+                farmerName: "Farmer Name",
+                phone: "Phone",
+                currentCrops: "Current Crops",
+                variants: "Variants",
+                farmSize: "Farm Size",
+                produceQuantity: "Quantity of produce",
+                farmLocation: "Farm Location",
+                otherDetails: "Other Details",
+            };
         } else if (selectedLanguage === "tel") {
-            csvFileName = "Data_v3_mapped.csv";
+            fileName = "Data_v3_mapped.xlsx";
+            // Define column names for Telugu language
+            columnNames = {
+                farmerName: "Farmer Name",
+                phone: "ఫోన్",
+                currentCrops: "ప్రస్తుత పంటలు",
+                variants: "వైవిధ్యాలు",
+                farmSize: "వ్యవసాయ పరిమాణం",
+                produceQuantity: "ఉత్పత్తి పరిమాణం",
+                farmLocation: "వ్యవసాయ స్థానం",
+                otherDetails: "ఇతర వివరాలు",
+            };
         }
 
-        fetch(csvFileName)
-            .then((response) => response.text())
+        fetch(fileName)
+            .then((response) => {
+                if (fileName.endsWith(".csv")) {
+                    return response.text();
+                } else if (fileName.endsWith(".xlsx")) {
+                    return response.arrayBuffer();
+                } else {
+                    throw new Error("Unsupported file format");
+                }
+            })
             .then((data) => {
-                const dataArray = Papa.parse(data, { header: true }).data;
+                let dataArray;
+                if (fileName.endsWith(".csv")) {
+                    dataArray = Papa.parse(data, { header: true }).data;
+                } else if (fileName.endsWith(".xlsx")) {
+                    const workbook = XLSX.read(data, { type: "array" });
+                    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+                    dataArray = XLSX.utils.sheet_to_json(worksheet);
+                }
 
-                // Populate the dropdown with Farmer Names from the CSV data
+                // Populate the dropdown with Farmer Names from the data
                 dataArray.forEach((item) => {
                     const option = document.createElement("option");
-                    option.value = item["Farmer Name"];
-                    option.textContent = item["Farmer Name"];
+                    option.value = item[columnNames.farmerName];
+                    option.textContent = item[columnNames.farmerName];
                     farmerNameDropdown.appendChild(option);
                 });
 
@@ -30,16 +68,16 @@ document.addEventListener("DOMContentLoaded", function () {
                 farmerNameDropdown.addEventListener("change", function () {
                     const selectedFarmer = this.value;
                     const selectedData = dataArray.find(
-                        (item) => item["Farmer Name"] === selectedFarmer
+                        (item) => item[columnNames.farmerName] === selectedFarmer
                     );
                     if (selectedData) {
-                        farmerDetails.innerHTML = `<p><strong>Phone:</strong> ${selectedData["Phone"]}</p>
-                                                  <p><strong>Current Crops:</strong> ${selectedData["Current Crops"]}</p>
-                                                  <p><strong>Variants:</strong> ${selectedData["Variants"]}</p>
-                                                  <p><strong>Farm Size:</strong> ${selectedData["Farm Size"]}</p>
-                                                  <p><strong>Quantity of produce:</strong> ${selectedData["Quantity of produce"]}</p>
-                                                  <p><strong>Farm Location:</strong> ${selectedData["Farm Location"]}</p>
-                                                  <p><strong>Other Details:</strong> ${selectedData["Other Details"]}</p>`;
+                        farmerDetails.innerHTML = `<p><strong>${columnNames.phone}:</strong> ${selectedData[columnNames.phone]}</p>
+                                                  <p><strong>${columnNames.currentCrops}:</strong> ${selectedData[columnNames.currentCrops]}</p>
+                                                  <p><strong>${columnNames.variants}:</strong> ${selectedData[columnNames.variants]}</p>
+                                                  <p><strong>${columnNames.farmSize}:</strong> ${selectedData[columnNames.farmSize]}</p>
+                                                  <p><strong>${columnNames.produceQuantity}:</strong> ${selectedData[columnNames.produceQuantity]}</p>
+                                                  <p><strong>${columnNames.farmLocation}:</strong> ${selectedData[columnNames.farmLocation]}</p>
+                                                  <p><strong>${columnNames.otherDetails}:</strong> ${selectedData[columnNames.otherDetails]}</p>`;
                     } else {
                         farmerDetails.innerHTML = "";
                     }
